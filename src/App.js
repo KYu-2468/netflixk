d2ba9591f9d3ecfd391d2f5da6729100c95223c6
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-10-02 23:37:45 -0700

    Add AWS authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+// import Movies from "./components/Movies";
+
+// AWS Authenticator
+import { withAuthenticator, Button, Heading } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 const URL = "https://api.themoviedb.org/3";
 const API_KEY = "Put your API key here";
@@ -13,19 +18,34 @@ const endpoints = {
   upcoming: "/movie/upcoming",
 };
 
-function App() {
+function App({ signOut, user }) {
   const [originals, setOriginals] = useState([]);
   const [trending, setTrending] = useState([]);
   const [nowPlaying, setNowPlaying] = useState([]);
   const [popular, setPopular] = useState([]);
   const [topRated, setTopRated] = useState([]);
   const [upcoming, setUpcoming] = useState([]);
+  console.log(user, signOut);
+  // useEffect(() => {
+  //   // Load Originals
+  //   axios
+  //     .get(`${URL}${endpoints.originals}`, {
+  //       params: {
+  //         api_key: API_KEY,
+  //       },
+  //     })
+  //     .then((res) => setOriginals(res.data.results));
+
+  //   // Get other categories with the same pattern here
+  // }, []);
 
   return (
     <>
-      <h1>Hello world!</h1>
+      {/* <Movies title="Netflix originals" movies={originals} /> */}
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
     </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/components/Movies.styles.js
@@ -0,0 +1,10 @@
+import tw, { styled } from "twin.macro";
+
+export const MoviesTitle = styled.h2`
+  ${tw`
+      text-2xl
+      font-bold
+      uppercase
+      mx-8
+    `}
+`;
